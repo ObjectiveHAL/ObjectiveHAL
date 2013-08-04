2975f4953d46f4ea2d4e74a775c5330fb98e1686
Bennett Smith <bennett@focalshift.com>
2013-08-03 21:48:53 -0700

    Attempts to persuade xctool to emit shorter divider lines.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -4,3 +4,5 @@ set -e
 brew update
 brew install xctool
 
+# This hack is here to make sure that the xctool dividers fit.
+stty columns 60
--- travis/script.sh
+++ travis/script.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 set -e
 
-echo TRAVIS=$TRAVIS
 xctool -workspace ObjectiveHAL.xcworkspace -scheme ObjectiveHAL -sdk iphonesimulator build test
