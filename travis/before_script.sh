d282f55f7de029b558951a86faab4147f0303e2c
Bennett Smith <bennett@focalshift.com>
2013-08-12 13:44:56 -0700

    Updates to before_script now add lighttpd to the list of apps the firewall allows connections from.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -8,6 +8,13 @@ brew update
 brew install xctool
 brew install lighttpd
 
+# Add lighttpd to the list of allowed applications to accept incoming network connections.
+/usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+/usr/libexec/ApplicationFirewall/socketfilterfw -s /usr/local/sbin/lighttpd
+/usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate off
+/usr/libexec/ApplicationFirewall/socketfilterfw --add /usr/local/sbin/lighttpd
+/usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+
 # This hack is here to make sure that the xctool dividers fit.
 stty columns 60
 
