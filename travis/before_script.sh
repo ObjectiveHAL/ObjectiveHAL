012b7bb607cc1dcee9cde8f8d024c9d538518c7d
Bennett Smith <bennett@focalshift.com>
2013-09-21 23:56:04 -0700

    Fixes path to lighttpd.

--- travis/before_script.sh
+++ travis/before_script.sh
@@ -13,7 +13,7 @@ brew install lighttpd
 brew install appledoc
 
 # Launch Lighty in the background. It serves test fixtures at port 7100.
-/usr/local/sbin/lighttpd -f lighttpd/lighttpd.conf
+/usr/local/opt/lighttpd -f lighttpd/lighttpd.conf
 
 # This hack is here to make sure that the xctool dividers fit.
 stty columns 60
