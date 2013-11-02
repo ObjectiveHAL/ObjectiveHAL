982bc47273393538fb34924ffde1a1961dbc9048
Bennett Smith <bennett@focalshift.com>
2013-11-02 11:53:38 -0700

    Turns off travis build until the support Xcode 5.

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 
-xctool -workspace ObjectiveHAL.xcworkspace -scheme ObjectiveHAL -sdk iphonesimulator build test
+echo "Build cannot complete - Travis does not support iOS 7 / Xcode 5 yet."
+# xctool -workspace ObjectiveHAL.xcworkspace -scheme ObjectiveHAL -sdk iphonesimulator build test
